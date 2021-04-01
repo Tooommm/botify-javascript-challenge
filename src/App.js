94203d7cbcbb05d4bcc0de863aa46544fa54aca0
Thomas Brouillet <thomasbrouillet@gmail.com>
2021-04-01 14:59:53 +0200

    fetching data from the NASA api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import axios from "axios";
+import setUpData from "./helpers/setUpData";
 
 function App() {
+  const [data, setData] = useState([]); //store data from the NASA api
+
+  useEffect(() => {
+    const fecthData = async () => {
+      await axios
+        .get("https://api.nasa.gov/neo/rest/v1/neo/browse?", {
+          params: {
+            api_key: "DEMO_KEY",
+          },
+        })
+        .then((response) =>
+          // store information in data using helpers setUpData
+          setData(setUpData(response.data.near_earth_objects))
+        )
+        .catch((error) => {
+          //handle error by console log error response from the NASA api
+          if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            console.log(error.request);
+          } else {
+            console.log("Error", error.message);
+          }
+          console.log(error.config);
+        });
+    };
+    fecthData();
+  }, []);
+
+  console.log(data);
   return <div>dataChart</div>;
 }
 
