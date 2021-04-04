c0fb417252a57bec0075e70165db8882bef726f1
Thomas Brouillet <thomasbrouillet@gmail.com>
2021-04-04 09:33:55 +0200

    comment and styling table

--- src/components/NeoTable.js
+++ src/components/NeoTable.js
@@ -1,13 +1,19 @@
+import "./NeoTable.css";
+
 import React from "react";
 import Table from "react-bootstrap/Table";
 
 const NeoTable = ({ data }) => {
+  // define the table body
   const renderBody = () => {
     return (
       data &&
-      data.map(({ name, min, max }) => {
+      data.map(({ name, min, max }, index) => {
+        const id = index + 1; //Define an id
+
         return (
-          <tr key={name}>
+          <tr key={id}>
+            <td>{id}</td>
             <td>{name}</td>
             <td>{Math.round(min * 100) / 100}</td>
             <td>{Math.round(max * 100) / 100}</td>
@@ -18,9 +24,10 @@ const NeoTable = ({ data }) => {
   };
 
   return (
-    <Table striped bordered hover size="sm">
+    <Table striped bordered hover size="sm" id="neo-table">
       <thead>
         <tr>
+          <th></th>
           <th>Neo Name</th>
           <th>Min Estimated Diameter (km)</th>
           <th>Max estimated Diameter</th>
